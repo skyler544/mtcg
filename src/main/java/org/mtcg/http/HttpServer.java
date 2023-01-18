685e9bb50192b738a5750019cf0b41d645245622
Skyler Mayfield <skyler544@gmail.com>
2023-01-18 20:27:59 +0100

    make connections thread-capable

--- src/main/java/org/mtcg/http/HttpServer.java
+++ src/main/java/org/mtcg/http/HttpServer.java
@@ -9,6 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class HttpServer {
 
@@ -20,54 +21,64 @@ public class HttpServer {
     }
 
     public void start() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
         try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
             while (true) {
-                try (final Socket socket = serverSocket.accept()) {
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream()));
+                Runnable httpRequest = () -> {
+                    try (final Socket socket = serverSocket.accept()) {
+                        BufferedReader br = new BufferedReader(
+                                new InputStreamReader(socket.getInputStream()));
 
-                    final RequestContext requestContext = parseInput(br);
-                    System.out.println("Thread: " + Thread.currentThread().getName());
-                    requestContext.print();
-                    final RouteIdentifier routeIdentifier = new RouteIdentifier(
-                            requestContext.getPath(),
-                            requestContext.getHttpVerb());
-                    final Route route = router.findRoute(routeIdentifier);
-                    Response response;
-                    try {
-                        // This works like a for-each for the methods of the
-                        // route. This means that in order to use this for any
-                        // route we need to follow the pattern given by the
-                        // signature of the method when writing the route
-                        // methods.
-                        response = route.process(requestContext);
-                        response = new Response();
-                    } catch (BadRequestException badRequestException) {
-                        response = new Response();
-                        response.setBody(badRequestException.getMessage());
-                        response.setHttpStatus(HttpStatus.BAD_REQUEST);
-                    } catch (IllegalStateException e) {
-                        response = new Response();
-                        response.setBody(e.getMessage());
-                        response.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+                        final RequestContext requestContext = parseInput(br);
+                        System.out.println("Thread: " + Thread.currentThread().getName());
+                        requestContext.print();
+                        final RouteIdentifier routeIdentifier = new RouteIdentifier(
+                                requestContext.getPath(),
+                                requestContext.getHttpVerb());
+                        final Route route = router.findRoute(routeIdentifier);
+                        Response response;
+                        try {
+                            // This works like a for-each for the methods of the
+                            // route. This means that in order to use this for any
+                            // route we need to follow the pattern given by the
+                            // signature of the method when writing the route
+                            // methods.
+                            // response = route.process(requestContext);
+                            response = new Response();
+                            response.setBody("Hello there");
+                            response.setHttpStatus(HttpStatus.OK);
+                        } catch (BadRequestException badRequestException) {
+                            response = new Response();
+                            response.setBody(badRequestException.getMessage());
+                            response.setHttpStatus(HttpStatus.BAD_REQUEST);
+                        } catch (IllegalStateException e) {
+                            response = new Response();
+                            response.setBody(e.getMessage());
+                            response.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+                        }
+
+                        BufferedWriter w = new BufferedWriter(
+                                new OutputStreamWriter(socket.getOutputStream()));
+                        // 200 OK
+                        w.write("HTTP/1.1 ");
+                        w.write(response.getHttpStatus().getStatusCode() + " ");
+                        w.write(response.getHttpStatus().getStatusMessage());
+                        w.newLine();
+                        // write headers
+                        w.newLine();
+                        // write body
+                        w.flush();
+                    } catch (IOException e) {
+                        System.err.println(e);
                     }
-
-                    BufferedWriter w = new BufferedWriter(
-                            new OutputStreamWriter(socket.getOutputStream()));
-                    // 200 OK
-                    w.write("HTTP/1.1 ");
-                    w.write(response.getHttpStatus().getStatusCode() + " ");
-                    w.write(response.getHttpStatus().getStatusMessage());
-                    w.newLine();
-                    // write headers
-                    w.newLine();
-                    // write body
-                    w.flush();
-                }
+                };
+                executorService.execute(httpRequest);
             }
         } catch (IOException e) {
             System.err.println(e);
         }
+
+        executorService.shutdown();
     }
 
     public RequestContext parseInput(BufferedReader bufferedReader) throws IOException {

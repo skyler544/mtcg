26a7ed9887a1fd59151c0a877d8e0395e4561ec4
Skyler Mayfield <skyler544@gmail.com>
2023-02-11 13:11:23 +0100

    add unit tests for happy/sad paths on POST /users

--- src/test/java/org/mtcg/application/controller/RestUserControllerTest.java
+++ src/test/java/org/mtcg/application/controller/RestUserControllerTest.java
@@ -1,18 +1,56 @@
 package org.mtcg.application.controller;
 
-import org.mtcg.application.repository.UserRepository;
+import org.mtcg.application.model.User;
+import org.mtcg.application.model.Credentials;
+import org.mtcg.application.service.UserService;
+import org.mtcg.http.BadRequestException;
+import org.mtcg.http.HttpStatus;
+import org.mtcg.http.Response;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 public class RestUserControllerTest {
-
     @Mock
-    private UserRepository userRepository;
+    private UserService userService;
 
     @InjectMocks
     private RestUserController restUserController;
 
+    @Test
+    void testRegisterUserSuccessfully() {
+        // Arrange
+        final var credentials = new Credentials("foo", "bar");
+
+        // Act
+        Response response = restUserController.register(credentials);
+
+        // Assert
+        assertEquals(HttpStatus.CREATED, response.getHttpStatus());
+    }
+
+    @Test
+    void testRegisterExistingUserFailure() {
+        // Arrange
+        boolean thrown = false;
+        final var credentials = new Credentials("foo", "bar");
+        when(userService.findUserByUsername(credentials.getUsername()))
+            .thenReturn(new User(credentials.getUsername(), credentials.getPassword()));
+
+        // Act
+        try {
+            restUserController.register(credentials);
+        } catch (BadRequestException e) {
+            thrown = true;
+        }
+
+        // Assert
+        assertTrue(thrown);
+    }
 }

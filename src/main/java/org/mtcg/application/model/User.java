82d7895ecce66f778424c8253379a95370e5c113
Skyler Mayfield <skyler544@gmail.com>
2023-02-11 19:50:27 +0100

    expand user model with name/bio/image

--- src/main/java/org/mtcg/application/model/User.java
+++ src/main/java/org/mtcg/application/model/User.java
@@ -3,8 +3,11 @@ package org.mtcg.application.model;
 public class User {
     private String token;
     private Credentials credentials;
+    private String name;
+	private String bio;
+	private String image;
 
-    public User(Credentials credentials) {
+	public User(Credentials credentials) {
         this.credentials = credentials;
         this.setToken();
     }
@@ -17,12 +20,36 @@ public class User {
         this.credentials = credentials;
     }
 
+    public String getToken() {
+        return token;
+    }
+
     // If we were serious about this, the token would be a hash
     private void setToken() {
         this.token = this.credentials.getUsername() + "-mtcgToken";
     }
 
-    public String getToken() {
-        return token;
-    }
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+    public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
 }

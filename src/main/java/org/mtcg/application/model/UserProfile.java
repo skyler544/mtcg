c71b1903f8ab1343a6937259bd2938f1538385a3
Skyler Mayfield <skyler544@gmail.com>
2023-02-12 12:12:00 +0100

    remove unused setter methods

--- src/main/java/org/mtcg/application/model/UserProfile.java
+++ src/main/java/org/mtcg/application/model/UserProfile.java
@@ -23,23 +23,11 @@ public class UserProfile {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getBio() {
         return bio;
     }
 
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
     public String getImage() {
         return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
     }
 }

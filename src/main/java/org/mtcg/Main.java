31f43f0cf27a7a710b019479a79c752e50e712be
Skyler Mayfield <skyler544@gmail.com>
2023-02-18 19:55:08 +0100

    implement POST /packages

--- src/main/java/org/mtcg/Main.java
+++ src/main/java/org/mtcg/Main.java
@@ -1,8 +1,11 @@
 package org.mtcg;
 
 import org.mtcg.application.controller.RestUserController;
+import org.mtcg.application.controller.RestCardController;
 import org.mtcg.application.repository.PostgresUserRepository;
 import org.mtcg.application.router.Router;
+import org.mtcg.application.service.CardService;
+import org.mtcg.application.repository.PostgresCardRepository;
 import org.mtcg.application.service.UserService;
 
 import org.mtcg.http.HttpServer;
@@ -12,9 +15,15 @@ public class Main {
         UserService userService = new UserService(new PostgresUserRepository());
         RestUserController restUserController = new RestUserController(userService);
 
+        CardService cardService = new CardService(new PostgresCardRepository());
+        RestCardController restCardController = new RestCardController(userService, cardService);
+
         Router router = new Router();
 
         restUserController.listRoutes()
+                .forEach(router::registerRoute);
+
+        restCardController.listRoutes()
                 .forEach(router::registerRoute);
 
         HttpServer httpServer = new HttpServer(router);

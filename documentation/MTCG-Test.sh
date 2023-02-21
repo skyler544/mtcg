b8a1b209e2af6283e89d6db1600c59cd2cdf5dec
Skyler Mayfield <skyler544@gmail.com>
2023-02-21 11:22:24 +0100

    add test for duplicate trading deal

--- documentation/MTCG-Test.sh
+++ documentation/MTCG-Test.sh
@@ -281,6 +281,9 @@ echo "check trading deals"
 curl -i -X GET http://localhost:10001/tradings --header "Authorization: Bearer kienboec-mtcgToken"
 echo .
 echo "create trading deal"
+curl -i -X POST http://localhost:10001/tradings --header "Content-Type: application/json" --header "Authorization: Bearer altenhof-mtcgToken" -d "{\"Id\": \"6cd85277-4590-49d4-b0cf-ba0a921faad0\", \"CardToTrade\": \"1cb6ab86-bdb2-47e5-b6e4-68c5ab389334\", \"Type\": \"monster\", \"MinimumDamage\": 15}"
+echo .
+echo "test create duplicate trading deal"
 curl -i -X POST http://localhost:10001/tradings --header "Content-Type: application/json" --header "Authorization: Bearer kienboec-mtcgToken" -d "{\"Id\": \"6cd85277-4590-49d4-b0cf-ba0a921faad0\", \"CardToTrade\": \"1cb6ab86-bdb2-47e5-b6e4-68c5ab389334\", \"Type\": \"monster\", \"MinimumDamage\": 15}"
 echo .
 
